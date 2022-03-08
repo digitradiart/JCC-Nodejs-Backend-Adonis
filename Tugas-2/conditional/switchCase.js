4b697eb5ad363a2cf1b08384a212b478283b1431
digitradiart <digitradiart@gmail.com>
2022-03-08 23:13:49 +0700

    switch case

--- Tugas-2/conditional/switchCase.js
+++ Tugas-2/conditional/switchCase.js
@@ -15,6 +15,50 @@ var tanggal; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 3
 var bulan; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200) 
 */
-var hari = 21;
-var bulan = 1;
+
+var tanggal = 1;
+var bulan = 15;
 var tahun = 1945;
+
+switch (bulan) {
+  case 1:
+    bulan = 'Januari';
+    break;
+  case 2:
+    bulan = 'Februari';
+    break;
+  case 3:
+    bulan = 'Maret';
+    break;
+  case 4:
+    bulan = 'April';
+    break;
+  case 5:
+    bulan = 'Mei';
+    break;
+  case 6:
+    bulan = 'Juni';
+    break;
+  case 7:
+    bulan = 'Juli';
+    break;
+  case 8:
+    bulan = 'Agustus';
+    break;
+  case 9:
+    bulan = 'September';
+    break;
+  case 10:
+    bulan = 'Oktober';
+    break;
+  case 11:
+    bulan = 'November';
+    break;
+  case 12:
+    bulan = 'Desember';
+    break;
+  default:
+    console.log(`Maaf tidak ada bulan ke-${bulan}.`);
+}
+
+console.log(`${tanggal} ${bulan} ${tahun}`);
