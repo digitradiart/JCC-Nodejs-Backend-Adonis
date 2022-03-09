60034d0c2d36f95c431a437c5e263e3e4040652a
digitradiart <digitradiart@gmail.com>
2022-03-09 07:51:52 +0700

    validasi tanggal dan tahun

--- Tugas-2/conditional/switchCase.js
+++ Tugas-2/conditional/switchCase.js
@@ -16,49 +16,54 @@ var bulan; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200) 
 */
 
-var tanggal = 1;
-var bulan = 15;
-var tahun = 1945;
+var tanggal = 9;
+var bulan = 13;
+var tahun = 2302;
 
-switch (bulan) {
-  case 1:
-    bulan = 'Januari';
-    break;
-  case 2:
-    bulan = 'Februari';
-    break;
-  case 3:
-    bulan = 'Maret';
-    break;
-  case 4:
-    bulan = 'April';
-    break;
-  case 5:
-    bulan = 'Mei';
-    break;
-  case 6:
-    bulan = 'Juni';
-    break;
-  case 7:
-    bulan = 'Juli';
-    break;
-  case 8:
-    bulan = 'Agustus';
-    break;
-  case 9:
-    bulan = 'September';
-    break;
-  case 10:
-    bulan = 'Oktober';
-    break;
-  case 11:
-    bulan = 'November';
-    break;
-  case 12:
-    bulan = 'Desember';
-    break;
-  default:
-    console.log(`Maaf tidak ada bulan ke-${bulan}.`);
+if (!(tanggal >= 1 && tanggal <= 31)) {
+  console.log('Tanggal harus di antara 1 - 31');
+} else if (!(tahun >= 1900 && tahun <= 2200)) {
+  console.log('Tahun harus di antara 1900 - 2200');
+} else {
+  switch (bulan) {
+    case 1:
+      bulan = 'Januari';
+      break;
+    case 2:
+      bulan = 'Februari';
+      break;
+    case 3:
+      bulan = 'Maret';
+      break;
+    case 4:
+      bulan = 'April';
+      break;
+    case 5:
+      bulan = 'Mei';
+      break;
+    case 6:
+      bulan = 'Juni';
+      break;
+    case 7:
+      bulan = 'Juli';
+      break;
+    case 8:
+      bulan = 'Agustus';
+      break;
+    case 9:
+      bulan = 'September';
+      break;
+    case 10:
+      bulan = 'Oktober';
+      break;
+    case 11:
+      bulan = 'November';
+      break;
+    case 12:
+      bulan = 'Desember';
+      break;
+    default:
+      console.log(`Maaf tidak ada bulan ke-${bulan}.`);
+  }
+  console.log(`${tanggal} ${bulan} ${tahun}`);
 }
-
-console.log(`${tanggal} ${bulan} ${tahun}`);
