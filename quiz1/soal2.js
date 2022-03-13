0b73395d6060d16088b2c36cd16b80f0796ad452
digitradiart <digitradiart@gmail.com>
2022-03-14 00:02:25 +0700

    soal 2

--- quiz1/soal2.js
+++ quiz1/soal2.js
@@ -29,7 +29,40 @@ console.log(kelompokAngka([]))
 //output:[[],[],[]]
 
  */
-
 function kelompokAngka(arr) {
   //Code disini
+
+  var hasil = [];
+
+  var ganjil = [];
+
+  var genap = [];
+
+  var kelipatanTiga = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+      kelipatanTiga.push(arr[i]);
+    } else if (arr[i] % 2 === 1 && arr[i] % 3 !== 0) {
+      ganjil.push(arr[i]);
+    } else if (arr[i] % 3 !== 0 && arr[i] % 2 === 0) {
+      genap.push(arr[i]);
+    }
+  }
+
+  hasil = [ganjil, genap, kelipatanTiga];
+
+  return hasil;
 }
+
+console.log(kelompokAngka([1, 2, 3, 4, 5, 6, 7]));
+
+//Output:[[1,5,7],[2,4],[3,6]]
+
+console.log(kelompokAngka([13, 27, 11, 15]));
+
+//Output:[[13,11],[],[27,15]]
+
+console.log(kelompokAngka([]));
+
+//output:[[],[],[]]
