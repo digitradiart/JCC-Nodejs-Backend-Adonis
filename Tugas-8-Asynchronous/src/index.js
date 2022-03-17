c34454a217b39eeedf148e2b4400f48637d4e503
digitradiart <digitradiart@gmail.com>
2022-03-17 08:14:03 +0700

    fs readFile and writeFile

--- Tugas-8-Asynchronous/src/index.js
+++ Tugas-8-Asynchronous/src/index.js
@@ -1,4 +1,4 @@
-import readData from './lib/fscallback';
+import { readData, writeData } from './lib/fscallback';
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -6,6 +6,17 @@ const command = args[0];
 switch (command) {
   case 'readData':
     readData();
+    break;
+
+  case 'writeData':
+    let [nameRaw, kelasRaw] = args[1].split(',');
+    let [key1, val1] = nameRaw.split(':');
+    let [key2, val2] = kelasRaw.split(':');
+    let obj = {};
+    obj[key1] = val1;
+    obj[key2] = val2;
+    console.log(obj);
+    writeData(obj);
     break;
 
   default:
--- Tugas-8-Asynchronous/src/lib/fscallback.js
+++ Tugas-8-Asynchronous/src/lib/fscallback.js
@@ -1,8 +1,20 @@
 import fs from 'fs';
 
-export default () => {
-  fs.readFile('data.json', (err, data) => {
+const path = 'data.json';
+export const readData = () => {
+  fs.readFile(path, (err, data) => {
     if (err) console.log(err);
     console.log(JSON.parse(data));
+  });
+};
+
+export const writeData = (newData) => {
+  fs.readFile(path, (err, data) => {
+    if (err) console.log(err);
+    let dataArr = JSON.parse(data);
+    dataArr.push(newData);
+    fs.writeFile(path, JSON.stringify(dataArr), { encoding: 'utf-8' }, (err) => {
+      if (err) console.log(err);
+    });
   });
 };
