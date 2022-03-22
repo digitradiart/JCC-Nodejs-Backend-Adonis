f72164042ea54b805ee2213cc92e658f79fd4bac
digitradiart <digitradiart@gmail.com>
2022-03-22 08:35:56 +0700

    test user id

--- tugas11/routes/index.js
+++ tugas11/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:id', (req, res) => {
+  res.send(`Selamat datang ${req.params.id}. Terima kasih sudah menjadi user.`);
+});
 module.exports = router;
